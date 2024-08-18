7e443878f1fc98a02c102cc1b05ccdf7ec5cdbb6
FelixJZ17 <felix_jimenez9@hotmail.com>
2024-08-18 18:10:32 +0200

    proyecto tal cual va

--- formularios/form_maestro_design.py
+++ formularios/form_maestro_design.py
@@ -1,9 +1,12 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import font, PhotoImage
+from PIL import Image, ImageTk
 from config import *  ## aquí importo todas las constantes
 
 import util.util_ventana as util_ventana
 import util.util_imagenes as util_img
+
+import fontawesome as fa
 
 class FormularioMaestroDesign(tk.Tk):
 
@@ -14,6 +17,7 @@ class FormularioMaestroDesign(tk.Tk):
         self.perfil = util_img.leer_imagen('./imagenes/foto_perfil_felix.png', (100, 100))
         self.config_window()
         self.paneles()
+        self.controles_barra_superior()
         
     # Codigo para ordenar la configuracion inicial de la ventana    
     def config_window(self):
@@ -39,5 +43,33 @@ class FormularioMaestroDesign(tk.Tk):
         self.cuerpo_principal = tk.Frame(
             self, bg=COLOR_CUERPO_PRINCIPAL, width=150)
         self.cuerpo_principal.pack(side=tk.RIGHT, fill='both', expand=True)
+
+    # Controles de barra superior
+    def controles_barra_superior(self):
+        # Configuracion de la barra superior
+        font_awesome = font.Font(family='FontAwesome', size=12)
+
+        #Etiqueta de titulos
+        self.labelTitulo = tk.Label(self.barra_superior, text='FJ Gestiones')
+        self.labelTitulo.config(
+            fg='#fff', font=('Roboto', 15), bg=COLOR_BARRA_SUPERIOR, pady=10, width=16)
+        self.labelTitulo.pack(side=tk.LEFT)
+
+        # Botón menú lateral   ---> texto \uf0c9 es un codigo para representar simbolo de "Bars " --> al final descargo como assets porque no lee bien
+        # Cargar la imagen PNG
+        img = Image.open("imagenes/assets/bars-solid-blanco_400x400.png")
+        img = img.resize((20, 20), Image.Resampling.LANCZOS)  # Redimensionar si es necesario
+        self.photo = ImageTk.PhotoImage(img)  ##tengo que hacerlo con el self, ya que sino no me muestra la foto.
+        self.buttonMenuLateral = tk.Button(self.barra_superior, text='prueba', image=self.photo, font=font_awesome,
+                                           command=lambda: print("¡Botón presionado!"), bd=0, bg=COLOR_BARRA_SUPERIOR, fg='white')
+        self.buttonMenuLateral.pack(side=tk.LEFT)
+
+        # Etiqueta de información
+        self.labelInformacion = tk.Label(self.barra_superior, text='felix@fjgestiones.com')
+        self.labelInformacion.config(
+            fg='#fff', font=('Roboto', 10), bg=COLOR_BARRA_SUPERIOR, padx=10, width=20)
+        self.labelInformacion.pack(side=tk.RIGHT)
+
+
 
 
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+## Video que sigo instrucciones: 
+## https://www.youtube.com/watch?v=vNw-zLB7GLY&t=1790s
+## del canal de: Autodidacta
+
 from formularios.form_maestro_design import FormularioMaestroDesign
 
 app = FormularioMaestroDesign()
